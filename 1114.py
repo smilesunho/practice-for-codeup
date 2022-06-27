89157bbb054a5ec38533be6ea148dc509d84c4bd
smilesunho <404orange@naver.com>
2022-06-27 22:00:15 +0900

    codeup

---
+++ 1114.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''
+두 정수를 입력받아 합을 출력한다.
+'''
+import numpy as np
+a=list(map(int,input().strip().split(" ")))
+print(np.sum(a))
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ 1117.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+'''
+두 실수를 입력받아 두 실수의 곱을 출력하되 소수 둘째자리까지 출력하시오.
+'''
+import numpy as np
+a1,a2=input().strip().split(" ")
+a1=float(a1)
+a2=float(a2)
+print(np.round(a1*a2,2))
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
