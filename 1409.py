9addc4bc8dcf1bb6ebc688d4b43ca5cd8411db54
smilesunho <404orange@naver.com>
2022-07-16 23:56:32 +0900

    codeup

---
+++ 1409.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+'''
+첫째 줄에 숫자 10개가 차례대로 입력된다.
+
+둘째 줄에 k값이 입력된다. 
+
+k번째 숫자가 무엇이었는지 출력한다.
+'''
+a=input().strip().split(" ")
+b=input()
+b=int(b)
+print(a[b-1])
+
+
+    
+
+
+
+
+
+# In[ ]:
+
+
+
+
