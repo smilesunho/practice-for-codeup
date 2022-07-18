84e101bae1d1f929a8a5beaea47c231ae02b5634
smilesunho <404orange@naver.com>
2022-07-18 23:22:23 +0900

    codeup

---
+++ 1135.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''
+a가 b보다 크거나 같을 경우 1, 그렇지 않은 경우 0을 출력한다.
+
+'''
+
+a,b=input().split(" ")
+a=int(a)
+b=int(b)
+if a<b:
+    print("0")
+else:
+    print("1")
+
+
+# In[ ]:
+
+
+
+
